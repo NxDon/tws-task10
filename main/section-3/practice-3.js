c68f35f9275aeac9c22e82728e6a292905c1d712
NxDon <645104928@qq.com>
2017-08-16 23:48:56 +0800

    add some comment

--- main/section-3/practice-3.js
+++ main/section-3/practice-3.js
@@ -1,9 +1,9 @@
 const countSameElements = require("../section-2/practice-1")
 const createUpdatedCollections = require("./practice-2")
-
+//此任务代码只需调用之前编写的两个模块就可以完成，为了练习模块加载（其实是懒）,故不把代码粘贴过来而采用了模块引入
 module.exports = function createUpdatedCollection(collectionA, objectB) {
     let newArr = countSameElements(collectionA);
-    let summarizedArray = createUpdatedCollections(newArr,objectB);
-    return summarizedArray
+    return  createUpdatedCollections(newArr,objectB);
+
 }
 
--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,6 +1,6 @@
 const countSameElements = require("../section-2/practice-2")
 const createUpdatedCollections = require("./practice-2")
-
+//此任务代码只需调用之前编写的两个模块就可以完成，为了练习模块加载（其实是懒）,故不把代码粘贴过来而采用了模块引入
 module.exports = function createUpdatedCollection(collectionA, objectB) {
     let newArr = countSameElements(collectionA);
     return createUpdatedCollections(newArr,objectB);
